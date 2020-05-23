ff45c7bd87be3e80362bff55292b43f40d75b321
maicoltc <maicoltimanacardenas50@gmail.com>
2020-05-22 19:41:30 -0500

    Este es mi primer comit - E

---
+++ ejemplo.m
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("***************************************************");
+printf("Practica: configurar y Usar Github en Linux RHEL");
+printf("***************************************************");
+printf("Michael Timana Cardenas");
+return 0;
+}
